ea2df72977bdac7382bd2a6ed803adb86763c6f8
hsalas006 <hsalas006@github.com>
2019-03-10 16:30:50 -0600

    working with the models for the games

--- backend/controllers/memory.js
+++ backend/controllers/memory.js
@@ -1,13 +1,38 @@
 exports.postGame = async(req, res, next)=>{
-    const content = req.body.content;
+    const IDgame = req.body.IDgame;
+    const IDplayer1 = req.body.IDplayer1;
+    const IDplayer2 = req.body.IDplayer2;
+    const matrix = req.body.matrix;
+    const turn = req.body.turn;
+    const score = req.body.score;
+    const IDsession = req.body.IDsession;
     // if the response is positive and created
     res.status(201).json({ 
         message: 'se realizo un post exitosamente',
-        post: {content: content} 
+        post: {
+            IDgame: IDgame,
+            IDplayer1: IDplayer1,
+            IDplayer2: IDplayer2,
+            matrix: matrix,
+            turn: turn,
+            score: score,
+            IDsession: IDsession
+        } 
     });
 };
 
 exports.getGames = async(req, res, next)=>{
     // if the response is positive
-    res.status(200).json({ data });
+    res.status(200).json({ 
+        posts:[{
+            IDgame : '00001',
+            IDplayer1 : 'jugador1',
+            IDplayer2 : 'jugador2',
+            matrix : [],
+            turn : 'jugador1',
+            score : '10/12',
+            IDsession : '000001'                
+        }]
+        
+     });
 };
