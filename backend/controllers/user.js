8f31ecdf16792bf0e7ab44bc06d1f39b7cbb93cf
hsalas006 <hsalas006@github.com>
2019-03-11 09:22:37 -0600

    backend work user and auth

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,7 +7,7 @@ const User = require('../models/user');
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.');
+    const error = new Error('Error en la validacion de los datos');
     error.statusCode = 422;
     error.data = errors.array();
     throw error;
@@ -26,7 +26,7 @@ exports.signup = (req, res, next) => {
       return user.save();
     })
     .then(result => {
-      res.status(201).json({ message: 'User created!', userId: result._id });
+      res.status(201).json({ message: 'usuario creado', userId: result._id });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -43,7 +43,7 @@ exports.login = (req, res, next) => {
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        const error = new Error('A user with this email could not be found.');
+        const error = new Error('el email ya existe');
         error.statusCode = 401;
         throw error;
       }
@@ -52,7 +52,7 @@ exports.login = (req, res, next) => {
     })
     .then(isEqual => {
       if (!isEqual) {
-        const error = new Error('Wrong password!');
+        const error = new Error('contraseña incorrecta');
         error.statusCode = 401;
         throw error;
       }
@@ -61,7 +61,7 @@ exports.login = (req, res, next) => {
           email: loadedUser.email,
           userId: loadedUser._id.toString()
         },
-        'somesupersecretsecret',
+        'secretsecret',
         { expiresIn: '1h' }
       );
       res.status(200).json({ token: token, userId: loadedUser._id.toString() });
