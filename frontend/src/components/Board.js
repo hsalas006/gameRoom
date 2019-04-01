ce7e59f333490ddc8f429b6bdf669b2627ffe7c7
hsalas006 <hsalas006@github.com>
2019-04-01 12:55:35 -0600

    image show!

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -7,9 +7,10 @@ export default class Board extends React.Component{
     super(props);
     this.state = {
       
+      pic:'',
       'isWhite':true,
       
-      'grid':Array(8).fill().map(x => Array(8).fill("+")),
+      grid:Array(8).fill().map(x => Array(8).fill("+")),
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -23,8 +24,15 @@ export default class Board extends React.Component{
 
   handleClick(x,y){
     console.log('x--> ',x,'y--> ',y);
+    this.setState({pic: 'https://pngimage.net/wp-content/uploads/2018/05/bola-preta-png-5.png'})
+
   }
   render(){
+    const img = <img style={{width: 50, height: 50}} src='https://pngimage.net/wp-content/uploads/2018/05/bola-preta-png-5.png'/>;
+    const white = <img style={{width: 50, height: 50}} src='https://wwww.ishaanbhola.com/d/item/white-circle-pngc09-4dd3-a6e5-e4a33d437599/'/>;
+
+    
+
     
     const g = this.state.grid;
     
@@ -34,7 +42,7 @@ export default class Board extends React.Component{
             
             return (
                 <div className="boxGame" key={i+'_'+j} data-index={i+'_'+j} onClick={()=>this.handleClick(i,j)}>
-                  <div className= "innerGame" key={i+'*'+j}>{i+'_'+j}  
+                  <div className= "innerGame" key={i+'*'+j}><img style={{width: 50, height: 50}} src={this.state.pic}/>
                   </div>
                 </div>
                 )
--- frontend/src/components/Square.js
+++ frontend/src/components/Square.js
@@ -7,8 +7,24 @@ export default class Square extends React.Component{
   render(){
     const color_ = this.props.color;
     return (
-      <div className="row" onClick={this.props.handleClick} >
-        <div className= "inner">   
+      <div className="row"
+        style={{
+          overflow:'hidden',
+          width:'auto',
+          height:'25px',
+          backgroundColor:'#e4e4a1',
+          color:'red',
+          boarderColor: 'black',
+          border:".5px solid black"
+        }}
+      onClick={this.props.handleClick} >
+        <div className= ""
+          style={{color:color_,
+                  border:"1px solid",
+                  backgroundColor: color_,
+                  borderRadius: "50%",
+                  borderColor: color_,
+                  height:"25px"}} >
         </div>
       </div>
     )
