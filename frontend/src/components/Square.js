41ffeb65732af6a75bf047f71dad60611ed593f4
hsalas006 <hsalas006@github.com>
2019-04-01 09:17:30 -0600

    board, square and style responsive

--- frontend/src/components/Square.js
+++ frontend/src/components/Square.js
@@ -7,26 +7,10 @@ export default class Square extends React.Component{
   render(){
     const color_ = this.props.color;
     return (
-      <td
-        style={{
-          overflow:'hidden',
-          width:'auto',
-          height:'25px',
-          backgroundColor:'#e4e4a1',
-          color:'red',
-          boarderColor: 'black',
-          border:".5px solid black"
-        }}
-      onClick={this.props.handleClick} >
-        <div className= "box"
-          style={{color:color_,
-                  border:"1px solid",
-                  backgroundColor: color_,
-                  borderRadius: "50%",
-                  borderColor: color_,
-                  height:25}} >
+      <div className="row" onClick={this.props.handleClick} >
+        <div className= "inner">   
         </div>
-      </td>
+      </div>
     )
   }
 }
